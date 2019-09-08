bf0d7f10d6700fd61cd4c95bba1ac5da63758257
gregonrails <g314richards@gmail.com>
2019-09-08 22:06:11 +0900

    added switch and route to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about/" component={About} />
+        <Route exact path="/article/:slug" component={Article} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Article.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Article() {
+  return <div>this is page article</div>;
+}
