79706a58e05b10a33716ab45222137a2df41b7d0
gregonrails <g314richards@gmail.com>
2019-09-08 21:35:43 +0900

    added dependecy react router. added pages folder and the 6 pages for the site

---
+++ src/pages/Error.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Error() {
+  return (
+    <Link to="/" className="btn-primary">
+      return home
+    </Link>
+  );
+}
---
+++ src/pages/Services.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Services() {
+  return <div>this is page services</div>;
+}
